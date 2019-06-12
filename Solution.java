eed1fd1d254d8fec6afd35ee0bed064fa5b5c6a7
sdim-ua <ingersol@yandex.ua>
2019-06-12 23:11:40 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,24 @@
+/* 
+Хороши работнички
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Manager extends Employee {
+
+    }
+
+    public class Chief extends Employee {
+
+    }
+
+    public class Employee {
+
+    }
+
+    public class Secretary extends Employee {
+
+    }
+}
